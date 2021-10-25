c42e2d50803036ecc4d9ac6c1bdb3ad89d336c11
umairmohd8 <mohammed08@protonmail.com>
2021-10-25 12:33:41 +0530

    post and get

---
+++ WebApplication1/Controllers/RoverController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoverController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok("hellow world");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(payload["FL"]);
+
+            return Ok(jsonString);
+        }
+    }
+}
